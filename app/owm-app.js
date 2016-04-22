cce40e1ec2da61c0fc7832007baf03df57bac3d9
jon-wade <jonwadeuk@gmail.com>
2016-04-22 09:04:50 +0100

    add scaffold for basic routing app

---
+++ app/owm-app.js
@@ -0,0 +1,41 @@
+angular.module('OWMApp', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider
+            .when('/', {
+                templateUrl : 'home.html',
+                controller : 'HomeCtrl'
+            })
+            .when('/error', {
+                template : '<p>Error - Page Not Found</p>'
+            })
+            .when('/cities/:city', {
+                templateUrl : 'city.html',
+                controller : 'CityCtrl',
+                resolve : {
+                    city: function(owmCities, $route, $location) {
+                        var city = $route.current.params.city;
+                        if(owmCities.indexOf(city) === -1 ) {
+                            $location.path('/error');
+                            return;
+                        }
+                        return city;
+                    }
+                }
+            })
+    }])
+    .controller('HomeCtrl', ['$scope', function($scope) {
+        //empty for now
+    }])
+    .controller('CityCtrl', function($scope, city) {
+        $scope.city = city;
+    })
+    .run(function($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path('/error');
+        });
+    })
+    .value('owmCities', ['New York', 'Dallas', 'Chicago']);
+
+
+
+
